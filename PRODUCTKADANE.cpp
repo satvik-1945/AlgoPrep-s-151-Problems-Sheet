87f8bf1f2e2960484893c2dab92e2b3113efbe0c
satvik <satviktejas@gmail.com>
2023-07-04 08:39:05 +0530

    4 July

---
+++ PRODUCTKADANE.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) 
+    {
+        int pre = 1;
+        int suff = 1;
+        int n = nums.size();
+        int ans = INT_MIN;
+        for(int i = 0 ; i< n ; i++)
+        {
+            if(pre == 0)pre = 1;
+            if(suff == 0) suff = 1;
+            
+                pre = pre * nums[i];
+                suff = suff * nums[n-1 - i];
+                ans = max(ans, max(pre,suff));           
+        }
+        return ans;
+    }
+};
