c1dad3e1babf8ea387f74b2d7797d4642909f094
satvik <satviktejas@gmail.com>
2023-07-25 22:21:27 +0530

    25july

---
+++ .vscode/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void helper(int index , vector<vector<int>> &ans, vector<int> & nums)
+    {
+        if(index == nums.size())
+        {
+            ans.push_back(nums);
+            return ;
+        }
+        for(int i = index ; i< nums.size() ; i++)
+        {
+            swap (nums[index] ,  nums[i]);
+            helper(index + 1, ans, nums);
+            swap(nums[index], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) 
+    {
+        vector<vector<int>> ans;
+        int index = 0;
+        helper(index , ans , nums);
+        return ans;
+
+    }
+};
